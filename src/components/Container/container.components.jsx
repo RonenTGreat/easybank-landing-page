bf02b518e4d5efb70df1332695e43469c7cc62f3
RonenTGreat <ronenh53@gmail.com>
2023-04-30 23:26:26 +0000

    .

--- src/components/Container/container.components.jsx
+++ src/components/Container/container.components.jsx
@@ -1,9 +1,25 @@
+import mockups from "../../assets/images/image-mockups.png";
+
 const Container = () => {
-    return ( 
-        <main>
-            <div className=" absolute -z-10 top-20 bg-cover w-full h-[330px] bg-no-repeat bg-[url('./assets/images/bg-intro-mobile.svg')]"></div>
-        </main>
-     );
-}
- 
+  return (
+    <main>
+      <div className=" absolute top-8 -z-30 w-full h-[340px] bg-cover bg-no-repeat bg-[url('./assets/images/bg-intro-mobile.svg')]">
+        <img className=" absolute -top-[6rem]" src={mockups} alt="mockups" />
+      </div>
+
+      <section className=" absolute top-[25rem] grid place-items-center">
+        <h1 className=" text-4xl text-dark-blue text-center mb-3">
+          Next generation digital banking
+        </h1>
+        <p className=" text-center text-grayish-blue text-base font-light mb-10">
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <a href="/#" className=" bg-bright-cyan text-white py-4 px-8 rounded-full text-xl font-bold">Request Invite</a>
+      </section>
+    </main>
+  );
+};
+
 export default Container;
--- src/components/Header/header.components.jsx
+++ src/components/Header/header.components.jsx
@@ -5,21 +5,21 @@ import open from "../../assets/images/icon-hamburger.svg"
 import close from "../../assets/images/icon-close.svg"
 
 const Header = () => {
-    const [toggle, setToggle] = useState(true);
+    const [toggle, setToggle] = useState(false);
     
     function navHandle(){
        setToggle(!toggle);
     }
 
     return ( 
-        <header className=" px-5 py-8">
+        <header className=" px-5 py-6 relative z-0 bg-white">
             <div className=" flex justify-between">
                 <img src={logo} alt="logo" />
                 <img src={!toggle ? open : close} onClick={navHandle} alt="menu bar" />
             </div>
 
-            {toggle ? <nav className=" bg-white mt-11 rounded-md shadow-xl">
-                <ul className=" py-5 grid place-items-center gap-3">
+            {toggle ? <nav className=" absolute left-2 px-[8.6rem] bg-white mt-11 rounded-md shadow-xl">
+                <ul className=" py-6 grid place-items-center gap-3">
                     <li className=" text-lg font-bold text-dark-blue"><a href="/#">Home</a></li>
                     <li className=" text-lg font-bold text-dark-blue"><a href="/#">About</a></li>
                     <li className=" text-lg font-bold text-dark-blue"><a href="/#">Contact</a></li>
