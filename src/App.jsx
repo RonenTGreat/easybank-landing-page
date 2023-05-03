ede5714d2e9dcfcec23dfd88a4d5f832943ebbd8
RonenTGreat <ronenh53@gmail.com>
2023-05-03 13:16:50 +0000

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/header.components"
 import Container from "./components/Container/container.components"
+import Footer from "./components/Footer/footer.components"
 
 function App() {
   return (
     <>
       <Header />
       <Container />
+      <Footer />
 
     </>
   )
